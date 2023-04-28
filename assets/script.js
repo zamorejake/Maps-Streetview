3fed09512bfe4337d90ab7673ccfe39a6eda3b64
Jake <zamorejake@gmail.com>
2023-04-27 19:53:48 -0500

    Fixed issue with CORS, set 5 results to local storage, put first api call into 2nd

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
-let question = "yellow stone";
-var requestUrl = "https://developer.nps.gov/api/v1/parks?limit=5&q=" + question +"&api_key=zcVsmk0xbipvcZpXfFUHrZuNkcfpYUIRiuic2fh1";
-//var requestUrl2 = "https://www.mediawiki.org/w/api.php?action=query&prop=revisions&titles=API:Properties&rvprop=content&format=json";
+let question = "purple";
+let requestUrl = "https://developer.nps.gov/api/v1/parks?limit=5&q=" + question +"&api_key=zcVsmk0xbipvcZpXfFUHrZuNkcfpYUIRiuic2fh1";
 let example = [];
 fetch(requestUrl)
   .then(function (response) {
@@ -10,18 +9,30 @@ fetch(requestUrl)
        //console.log(data)
       for (i = 0; i < data.data.length; i++) {
         example.push(data.data[i].fullName);
+        localStorage.setItem("12345"[i], example[i]);
+
       }
       console.log(example[0]);
-  });
 
-  fetch(requestUrl2)
-  .then(function (response2) {
-    return response2.json();
-  })
-  .then(function (data2) {
-       console.log(data2)
-     /* for (i = 0; i < data.data.length; i++) {
-        example.push(data.data[i].fullName);
-        console.log(example[0]);
-      } */
+      //local storage stuff
+     // localStorage.setItem(example[0], example[0]);
+     //localStorage.getItem()
+
+
+
+      //2nd call
+      let requestUrl2 = "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search="+ example[0] + "&limit=5";
+      fetch(requestUrl2)
+      .then(function (response2) {
+        return response2.json();
+      })
+      .then(function (data2) {
+        console.log(data2)
+      });
+
+      //set items on page
+
+      
+
   });
+  
