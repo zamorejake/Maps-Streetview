81a14c99fef4bbff5aa2c52327d245c8541df8a3
afrazier01 <amfrazier01@gmail.com>
2023-04-27 19:53:16 -0500

    Test

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,29 @@
 let question = "yellow stone";
 var requestUrl = "https://developer.nps.gov/api/v1/parks?limit=5&q=" + question +"&api_key=zcVsmk0xbipvcZpXfFUHrZuNkcfpYUIRiuic2fh1";
-//var requestUrl2 = "https://www.mediawiki.org/w/api.php?action=query&prop=revisions&titles=API:Properties&rvprop=content&format=json";
+var requestUrl2 = "https://api.openweathermap.org/geo/1.0/direct?q=london&appid=489926538e0b86d709e90c069e0d5027";
 let example = [];
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-       //console.log(data)
-      for (i = 0; i < data.data.length; i++) {
-        example.push(data.data[i].fullName);
-      }
-      console.log(example[0]);
-  });
-
-  fetch(requestUrl2)
-  .then(function (response2) {
+    //console.log(data)
+    for (i = 0; i < data.data.length; i++) {
+    example.push(data.data[i].fullName);
+    }
+    console.log(example[0]);
+      
+      
+    fetch(requestUrl2)
+    .then(function (response2) {
     return response2.json();
-  })
-  .then(function (data2) {
+    })
+    .then(function (data2) {
        console.log(data2)
      /* for (i = 0; i < data.data.length; i++) {
         example.push(data.data[i].fullName);
         console.log(example[0]);
       } */
   });
+  });
+
