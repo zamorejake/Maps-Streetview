1f4f3db7a63ac4a7bb81b8dc6fddf0646452f3cb
afrazier01 <amfrazier01@gmail.com>
2023-05-02 10:21:14 -0500

    Cleaned up code in js file

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,6 @@ let submitButton = document.getElementById("submit");
 let searchValue = document.getElementById("search");
 let title = document.getElementById("title");
 
-
-//data usa api to test
-/*
-var dataUSAURL = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population'
-fetch(dataUSAURL)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      console.log(data.data[0]);
-});
-*/
-
-//button to trigger code
 submitButton.addEventListener("click", function (e) {
   e.preventDefault();
   var userSearch = searchValue.value;
@@ -143,73 +129,3 @@ function fetchAPI (userSearch) {
     clearBtnEl.remove();
   })
 }
-
-// let requestUrl =
-//     "https://developer.nps.gov/api/v1/parks?limit=1&q=" +
-//     userSearch +
-//     "&api_key=zcVsmk0xbipvcZpXfFUHrZuNkcfpYUIRiuic2fh1";
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       for (i = 0; i < data.data.length; i++) {
-//         example.push(data.data[i].fullName);
-//         localStorage.setItem("12345"[i], example[i]);
-//       }
-//       //local storage stuff
-
-//       //console.log(example[0]);
-//       //local storage stuff
-//       // localStorage.setItem(example[0], example[0]);
-//       //localStorage.getItem()
-
-//       //ny times call
-//       let nyUrl =
-//         "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-//         userSearch +
-//         "&api-key=6p6VhtV9RUQZuaPRE6AOQWL2K9IwE9Ef";
-
-//       fetch(nyUrl)
-//         .then(function (response3) {
-//           return response3.json();
-//         })
-//         .then(function (data3) {
-//           console.log(data3.response.docs[0].snippet);
-//           let container = document.createElement("h4");
-//           container.textContent = data3.response.docs[0].snippet;
-//           title.appendChild(container);
-//         });
-
-//       //3rd call
-//       for (i = 0; i < example.length; i++) {
-//         let requestUrl2 =
-//           "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=" +
-//           example[i] +
-//           "&limit=5";
-
-//         fetch(requestUrl2)
-//           .then(function (response2) {
-//             return response2.json();
-//           })
-//           .then(function (data2) {
-//             let container = document.createElement("a");
-//             container.href = data2[3][0];
-//             let lineBreak = document.createElement("br");
-//             container.innerText = "Click here for wiki link!";
-//             console.log(data2[3][0]);
-//             title.appendChild(container);
-//             title.appendChild(lineBreak);
-//           });
-//       }
-//       searchValue.value = "";
-//       let container = document.createElement("h4");
-//       container.setAttribute("id", "page");
-//       example.forEach(function (example) {
-//         let container = document.createElement("h4");
-//         container.textContent = example;
-//         title.appendChild(container);
-//       });
-//       console.log(example.length);
-//     });
