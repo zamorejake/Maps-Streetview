7a430a97241c56cdc94e6f7470935084e25efc2c
Jake <zamorejake@gmail.com>
2023-05-01 20:48:59 -0500

    got newyork times api working and displaying on screen, fixed wiki links, and limited to 1 result

--- assets/script.js
+++ assets/script.js
@@ -3,52 +3,62 @@ let submitButton = document.getElementById("submit");
 let searchValue = document.getElementById("search");
 let title = document.getElementById("title");
 
-let nyurl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=6p6VhtV9RUQZuaPRE6AOQWL2K9IwE9Ef'
+let nyurl =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=yellow&api-key=6p6VhtV9RUQZuaPRE6AOQWL2K9IwE9Ef";
 
 fetch(nyurl)
-        .then(function (response3) {
-          return response3;
-        })
-        .then(function (data3) {
-          console.log(data3);
-        });
+  .then(function (response3) {
+    return response3.json();
+  })
+  .then(function (data3) {
+    console.log(data3.response.docs[0].snippet);
+    let container = document.createElement("h4");
+    container.textContent = data3.response.docs[0].snippet;
+    title.appendChild(container);
+  });
 
 submitButton.addEventListener("click", function (e) {
   e.preventDefault();
   let question = searchValue.value;
   let requestUrl =
-        "https://developer.nps.gov/api/v1/parks?limit=5&q=" +
-        question +
-        "&api_key=zcVsmk0xbipvcZpXfFUHrZuNkcfpYUIRiuic2fh1";
+    "https://developer.nps.gov/api/v1/parks?limit=1&q=" +
+    question +
+    "&api_key=zcVsmk0xbipvcZpXfFUHrZuNkcfpYUIRiuic2fh1";
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      //console.log(data);
       for (i = 0; i < data.data.length; i++) {
         example.push(data.data[i].fullName);
         localStorage.setItem("12345"[i], example[i]);
       }
       //console.log(example[0]);
-
       //local storage stuff
       // localStorage.setItem(example[0], example[0]);
       //localStorage.getItem()
-
       //2nd call
-      let requestUrl2 =
-        "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=" +
-        example[0] +
-        "&limit=5";
+      for (i = 0; i < example.length; i++) {
+        let requestUrl2 =
+          "https://en.wikipedia.org//w/api.php?action=opensearch&format=json&origin=*&search=" +
+          example[i] +
+          "&limit=5";
 
-      fetch(requestUrl2)
-        .then(function (response2) {
-          return response2;
-        })
-        .then(function (data2) {
-          console.log(data2.url);
-        });
+        fetch(requestUrl2)
+          .then(function (response2) {
+            return response2.json();
+          })
+          .then(function (data2) {
+            let container = document.createElement("a");
+            container.href = data2[3][0];
+            let lineBreak = document.createElement("br");
+            container.innerText = "Click here for wiki link!";
+            console.log(data2[3][0]);
+            title.appendChild(container);
+            title.appendChild(lineBreak);
+          });
+      }
       searchValue.value = "";
       let container = document.createElement("h4");
       container.setAttribute("id", "page");
@@ -59,6 +69,4 @@ submitButton.addEventListener("click", function (e) {
       });
       console.log(example.length);
     });
-
-  //set items on page
 });
